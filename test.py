68d2bc59fcb12e44d6b268c4d4f3ec75ea1e953f
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2018-12-10 22:49:13 -0200

    Adding some (not automated) test.

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import requests
-
-ans = requests.post("http://127.0.0.1:8000/git/repository/", json=dict(url="git@github.com:cristianowa/gitinfo.git"))
+url="git@github.com:cristianowa/gitinfo.git"
+ans = requests.get("http://127.0.0.1:8000/git/repository/?url={}".format(url))
 ans.raise_for_status()
-ans = requests.post("http://127.0.0.1:8000/git/repository/1/update/")
+print(ans.json())
+if not ans.json():
+    ans = requests.post("http://127.0.0.1:8000/git/repository/", json=dict(url=url))
+    ans.raise_for_status()
+ans = requests.get("http://127.0.0.1:8000/git/repository/?url={}".format(url))
+ans.raise_for_status()
+repo_id = ans.json()[0]["id"]
+
+ans = requests.post("http://127.0.0.1:8000/git/repository/{}/update/".format(repo_id))
 ans.raise_for_status()
