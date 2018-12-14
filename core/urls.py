b62c28f5af263d2c2167490942607af7fda5e169
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2018-12-14 21:57:52 -0200

    Adding some presentation.
    It must improve a lot !

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r"^/", views.main),
     url(r'^commit/$', views.CommitList.as_view()),
     url(r'^commit/(?P<pk>[0-9]+)/$', views.GetUpdateDeleteCommit.as_view()),
     url(r'^repository/$', views.RepositoryList.as_view()),
     url(r'^repository/(?P<pk>[0-9]+)/$', views.GetUpdateDeleteRepository.as_view()),
     url(r'^repository/(?P<pk>[0-9]+)/update/', views.update_repository),
-    url(r'^graph/(?P<type>[a-z]+)/', views.bar_graph)
+    url(r'^graph/bar/(?P<type>[a-z]+)/', views.bar_graph),
+    url(r'^graph/pie/(?P<type>[a-z]+)/', views.pie_graph)
 ]
--- core/views.py
+++ core/views.py
@@ -51,6 +51,18 @@ def update_repository(request, pk, *args, **kwargs):
     repository.update()
     return JsonResponse({})
 
+def pie_graph(request, type):
+    from datetime import datetime, timedelta
+    repository = Repository.objects.get(url=request.GET["repository"])
+    days = request.GET.get("days") or 30
+    periodo_start = datetime.today() - timedelta(days=int(days))
+    queryset = Commit.objects.filter(repository=repository, date__gte=periodo_start)
+    if type == "log":
+        datasource = CommitsByDeveloperLog(queryset=queryset, fields=['commiter', 'add', 'sub', 'churn'])
+    else:
+        datasource = CommitsByDeveloper(queryset=queryset, fields=['commiter', 'add', 'sub', 'churn'])
+    chart = yui.PieChart(datasource)
+    return render(request, "graph.html", {"chart": chart})
 
 def bar_graph(request, type):
     from datetime import datetime, timedelta
@@ -63,6 +75,15 @@ def bar_graph(request, type):
     else:
         datasource = CommitsByDeveloper(queryset=queryset, fields=['commiter', 'add', 'sub', 'churn'])
     chart = yui.BarChart(datasource)
-    return render(request, "bar_graph.html",{"chart":chart})
+    return render(request, "graph.html",{"chart":chart})
+
+def main(request):
+    repos = Repository.objects.all()
+    repos = [dict(name=r.url,
+                  pie_normal_url="/git/graph/pie/normal/?repository={}".format(r.url),
+                  pie_log_url="/git/graph/pie/log/?repository={}".format(r.url),
+                  bar_normal_url="/git/graph/bar/log/?repository={}".format(r.url),
+                  bar_log_url="/git/graph/bar/log/?repository={}".format(r.url)) for r in repos]
+    return render(request, "main.html", dict(repos=repos))
 
 #TODO list/create ssh pub key so user can add it as deploy key to git
