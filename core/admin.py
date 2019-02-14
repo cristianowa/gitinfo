9299ed77c77d40f9dd0200bf2e852b86ec83c3cd
Cristiano Werner Araújo <cristianowerneraraujo@gmail.com>
2019-02-14 00:27:31 -0200

    Better admin. Search available now.

--- core/admin.py
+++ core/admin.py
@@ -3,12 +3,25 @@ from core.models import Commit, Submodule, CommitErrorType, Commiter, Repository
     CommitBlamePercentage
 # Register your models here.
 
-admin.site.register(Commit)
-admin.site.register(Commiter)
-admin.site.register(Repository)
+class CommitAdmin(admin.ModelAdmin):
+    search_fields = ["sha1", "commiter__email"]
+
+admin.site.register(Commit, CommitAdmin)
+class CommiterAdmin(admin.ModelAdmin):
+    search_fields = ["email"]
+admin.site.register(Commiter, CommiterAdmin)
+class RepositoryAdmin(admin.ModelAdmin):
+    search_fields = ["url"]
+admin.site.register(Repository, RepositoryAdmin)
 
 admin.site.register(CommitErrorType)
+class RepositoryAdmin(admin.ModelAdmin):
+    search_fields = ["url"]
 admin.site.register(Submodule)
 admin.site.register(CommitsMetrics)
-admin.site.register(Tag)
-admin.site.register(CommitBlamePercentage)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ["commit__sha", "commiter__email"]
+admin.site.register(Tag, TagAdmin)
+class CommitBlamePercentageAdmin(admin.ModelAdmin):
+    search_fields = ["commit__sha1", "commiter__email", "commit__repository__url"]
+admin.site.register(CommitBlamePercentage, CommitBlamePercentageAdmin)
